77811c0b39a1a880f46c7698f4d50344ca859c54
honzhe05 <honzhe05@gmail.com>
2025-12-14 22:38:42 +0800

    first git

---
+++ main.js
@@ -0,0 +1,38 @@
+let stores = {};
+
+fetch("stores.json")
+  .then(res => res.json())
+  .then(data => {
+    stores = data;
+    console.log("資料載入完成", stores);
+  });
+
+function getSelectedCategory() {
+  return document.querySelector('input[name="category"]:checked')?.value;
+}
+
+function randomPick(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+document.getElementById("StartBtn").addEventListener("click", () => {
+  if (!stores || Object.keys(stores).length === 0) {
+    return alert("資料尚未載入，請稍等...");
+  }
+
+  let cat = getSelectedCategory();
+  if (!cat) return alert("請選分類");
+  
+  if (cat === "all") {
+    const categories = Object.keys(stores);
+    // ["drink","food","snack","other"]
+    cat = randomPick(categories);
+  }
+
+  const list = stores[cat] ?? [];
+  if (list.length === 0) return alert("這個分類沒有店家");
+
+  const pick = randomPick(list);
+
+  alert(`今天抽到的是：${pick.name}（分類：${cat}）`);
+  window.open(pick.map);
+});
